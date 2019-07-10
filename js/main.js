2cb81b2a020c063a874d87b1aa525b4fce996477
Johnnybravo23 <johnnyjohannes.jr@yahoo.com>
2019-07-10 00:49:38 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -13,6 +13,18 @@ function eventListeners() {
     document.querySelector('.navBtn').addEventListener('click', function () {
         ui.showNav();
     })
+    //display modal
+    const links = document.querySelectorAll('.work-item__icon');
+
+    links.forEach(function (item) {
+        item.addEventListener('click', function (event) {
+            ui.showModal(event)
+        })
+    })
+    // hide modal
+    document.querySelector('.work-modal__close').addEventListener('click', function () {
+        ui.closeModal()
+    })
 }
 
 // constructor function
@@ -25,4 +37,20 @@ UI.prototype.hidePreloader = function () {
 }
 UI.prototype.showNav = function () {
     document.querySelector('.nav').classList.toggle('nav--show')
+}
+// show modal
+UI.prototype.showModal = function (event) {
+    event.preventDefault();
+    if (event.target.parentElement.classList.contains('work-item__icons'));
+    let id = event.target.parentElement.dataset.id
+
+    const modal = document.querySelector('.work-modal');
+    const modalItem = document.querySelector('.work-modal__item');
+
+    modal.classList.add('work-modal--show');
+    modalItem.style.backgroundImage = `url(images/work-${id}.jpg)`
+}
+// hide modal
+UI.prototype.closeModal = function () {
+    document.querySelector('.work-modal').classList.remove('work-modal--show')
 }
